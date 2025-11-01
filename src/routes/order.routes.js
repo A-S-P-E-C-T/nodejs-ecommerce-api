b24b6e80ae72c8403475ffadc48ce4148dfbef10
Pankaj Joshi <pj5283067@gmail.com>
2025-11-01 20:26:27 +0530

    feat: add order routes for managing user orders

---
+++ src/routes/order.routes.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+import {
+    createOrder,
+    getUserOrders,
+    getSingleOrder,
+    cancelOrder,
+    updateOrderStatus,
+    getAllOrder,
+} from "../controllers/order.controller.js";
+
+const orderRouter = Router();
+
+orderRouter.route("/create-order").post(verifyJWT, createOrder);
+orderRouter.route("/get-user-orders").get(verifyJWT, getUserOrders);
+orderRouter.route("/get-single-order/:orderId").get(verifyJWT, getSingleOrder);
+orderRouter.route("/cancel-order/:orderId").get(verifyJWT, cancelOrder);
+orderRouter
+    .route("/update-order-status/:orderId")
+    .patch(verifyJWT, updateOrderStatus);
+orderRouter.route("/get-all-orders").get(verifyJWT, getAllOrder);
+
+export { orderRouter };
