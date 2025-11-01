488be570a7379615ccd4a1eb28170a3d878957bf
Pankaj Joshi <pj5283067@gmail.com>
2025-11-01 20:26:21 +0530

    feat: add cart routes for managing user cart operations

---
+++ src/routes/cart.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+import {
+    addItemToCart,
+    getUserCart,
+    updateItemQuantity,
+    removeItemFromCart,
+    clearCart,
+} from "../controllers/cart.controllers.js";
+const cartRouter = Router();
+
+cartRouter.route("/add-product-to-cart").post(verifyJWT, addItemToCart);
+cartRouter.route("/get-cart").get(verifyJWT, getUserCart);
+cartRouter.route("/updateItemQunatity").patch(verifyJWT, updateItemQuantity);
+cartRouter
+    .route("/removeItemFromCart/:productId")
+    .post(verifyJWT, removeItemFromCart);
+cartRouter.route("/clearCart").delete(verifyJWT, clearCart);
+export { cartRouter };
